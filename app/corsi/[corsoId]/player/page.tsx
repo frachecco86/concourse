f0a4a4bb0af64618fabf31a95356d843059551b7
Francesco Checco <francescochecco@gmail.com>
2026-07-18 18:35:56 +0200

    fix: errore sintassi console.log

--- app/corsi/[corsoId]/player/page.tsx
+++ app/corsi/[corsoId]/player/page.tsx
@@ -149,6 +149,10 @@ export default function PlayerPage() {
         return c;
       });
 
+      console.log('DEBUG: quizPerCapitoli =', Object.keys(quizPerCapitoli));
+      console.log('DEBUG: capitoliConQuiz =', capitoliConQuiz.map((c: any) => c.titolo));
+      console.log('DEBUG: capitoliConQuiz has quiz =', capitoliConQuiz.map((c: any) => ({ titolo: c.titolo, hasQuiz: !!c.quiz })));
+
       if (cancelled) return;
       setCapitoli(capitoliConQuiz);
       setLoading(false);
